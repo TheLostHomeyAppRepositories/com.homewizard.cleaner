bee3db224d909569d725c3befe3b3bee79950241
smarthomesven <sven@dypodex.com>
2026-09-08 16:50:58 +0200

    Code cleanup

--- app.js
+++ app.js
@@ -3,32 +3,13 @@
 const Homey = require('homey');
 const axios = require('axios');
 
-module.exports = class CleanerApp extends Homey.App {
+module.exports = class HomewizardCleanerApp extends Homey.App {
 
   /**
    * onInit is called when the app is initialized.
    */
   async onInit() {
     this.log('HomeWizard Cleaner has been initialized');
-    // generate ID, random UUID
-    try {
-      const { randomUUID } = require('crypto');
-      let id = this.homey.settings.get('id');
-      if (!id) {
-        id = randomUUID();
-        this.homey.settings.set('id', id);
-      }
-      await axios.post('https://homey-apps-telemetry.vercel.app/api/installations', {
-        id: id,
-        appId: "com.homewizard.cleaner",
-        homeyPlatform: this.homey.platformVersion ? this.homey.platformVersion : 1,
-        appVersion: this.manifest.version,
-      }).catch(error => {
-        this.error('Error sending telemetry data:', error.message);
-      });
-    } catch (error) {
-      this.error('Error in onInit:', error.message);
-    }
   }
 
 };
